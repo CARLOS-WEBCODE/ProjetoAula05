954e732ea80e8b835d6d6bd8412d8c7427c40ea2
Antonio Barbosa <acrb1108@gmail.com>
2023-12-06 18:18:12 -0300

    Criando a classe funcionário.

---
+++ ProjetoAula05/Entities/Funcionario.cs
@@ -0,0 +1,48 @@
+﻿using ProjetoAula05.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProjetoAula05.Entities
+{
+    public class Funcionario
+    {
+        #region Atributos
+
+        private Guid _id;
+        private string _nome;
+        private string _cpf;
+        private string _matricula;
+        private DateTime _dataAdmissao;
+        private TipoContratacao _tipo;
+
+        #endregion 
+
+        #region Propriedades
+
+        public Guid Id
+        {
+            get => _id;
+            set { _id = value; }
+        }
+
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                var regex = new Regex("^[A-Za-zÀ-Üà-ü\\s]{6,150}$");
+
+                if (!regex.IsMatch(value))
+                    throw new ArgumentException("Nome do funcionário inválido.");
+
+                _nome = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ProjetoAula05/Enums/TipoContratacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula05.Enums
+{
+    /// <summary>
+    /// Tipos de contratação de funcionário
+    /// </summary>
+    public enum TipoContratacao
+    {
+        ESTAGIO = 1,
+        CLT = 2,
+        TERCEIRIZADO = 3
+    }
+}
