ffa137d52ac0aad8264efb655b88c9c4c44c75e7
Antonio Barbosa <acrb1108@gmail.com>
2023-12-07 15:40:49 -0300

    Incluindo repository LINQ

---
+++ ProjetoAula05/Repositories/FuncionarioAbstractRepository.cs
@@ -0,0 +1,34 @@
+﻿using ProjetoAula05.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula05.Repositories
+{
+    /// <summary>
+    /// Classe abstrata para definir os métodos do repositorio de funcionário
+    /// </summary>
+    public abstract class FuncionarioAbstractRepository
+    {
+        #region
+
+        protected List<Funcionario> Funcionarios { get; set; }
+
+        #endregion
+
+        #region Métodos abstratos
+
+        public abstract void Adicionar(Funcionario funcionario);
+        public abstract void Atualizar(Funcionario funcionario);
+        public abstract void Remover(Funcionario funcionario);
+
+        public abstract List<Funcionario> ObterTodos();
+        public abstract List<Funcionario> ObterPorNome(string nome);
+        public abstract List<Funcionario> ObterPorDataAdmissao(DateTime? dataMin, DateTime? dataMax);
+        public abstract Funcionario ObterPorId(Guid id);
+
+        #endregion
+    }
+}
---
+++ ProjetoAula05/Repositories/FuncionarioLAMBDARepository.cs
@@ -0,0 +1,47 @@
+﻿using ProjetoAula05.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula05.Repositories
+{
+    public class FuncionarioLAMBDARepository : FuncionarioAbstractRepository
+    {
+        public override void Adicionar(Funcionario funcionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Atualizar(Funcionario funcionario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Funcionario> ObterPorDataAdmissao(DateTime? dataMin, DateTime? dataMax)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Funcionario ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Funcionario> ObterPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Funcionario> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remover(Funcionario funcionario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
